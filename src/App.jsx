14cdef0e85f05e383bb1811228b1d992e5044e49
tusharsaliann <tusharsalian89@gmail.com>
2024-10-06 16:10:12 +0530

    Initial commit for my React app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Home from "./components/Home"; 
+import About from "./components/About";
+import PortFolio from "./Components/PortFolio";
+import Experiance from "./components/Experiance";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Home />
+        <About />
+        <PortFolio />
+        <Experiance />
+        <Contact />
+        <Footer />
+      </div>
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
